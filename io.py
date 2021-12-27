0cc7f441744b575cb8012888731d4e2fe6bac4a3
MohammedDjameleddine <45519718+MohammedDjameleddine@users.noreply.github.com>
2021-12-27 09:56:56 +0100

    fixed it

--- io.py
+++ io.py
@@ -7,7 +7,7 @@ def main():
     # Write a list of random names to the file
     with open("student_names.txt", "w") as f:
         for i in range(10):
-            f.write(random.choice(string.ascii_letters, 10))
+            f.write(''.join(random.choices(string.ascii_letters, k=10)+['\n']))
 
     # print first n lines in student_names.txt
     with open("student_names.txt", "r") as f:
@@ -17,22 +17,22 @@ def main():
     # print last n lines in student_names.txt
     with open("student_names.txt", "r") as f:
         lines = f.readlines()
-        for i in range(10):
+        for i in range(min(10, len(lines))):
             print(lines[-i])
 
     # check if name x is in student_names.txt
     with open("student_names.txt", "r") as f:
-        for line in f:
-            if "x" in line:
-                print(line)
-                break
-        print("x not found")
-
+            for line in f:
+                if "x" in line:
+                    print(line)
+                    break
+            print("x not found")
+    
     # generate files A.txt, B.txt, ..., Z.txt
     for i in range(26):
         with open(chr(ord("A") + i) + ".txt", "w") as f:
             f.write(chr(ord("A") + i))
-
-
+    
+    
 if __name__ == '__main__':
     main()
